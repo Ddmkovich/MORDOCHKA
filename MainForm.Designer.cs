1890eefcdb1dd2bf6cd7a926260d731bb33ac963
Ddmkovich <79242132+Ddmkovich@users.noreply.github.com>
2021-12-15 16:47:56 +0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MORDOCHKA
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dgvClient = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClient
+            // 
+            this.dgvClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClient.Location = new System.Drawing.Point(12, 12);
+            this.dgvClient.Name = "dgvClient";
+            this.dgvClient.Size = new System.Drawing.Size(1200, 598);
+            this.dgvClient.TabIndex = 0;
+            this.dgvClient.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvClient_RowsAdded);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 691);
+            this.Controls.Add(this.dgvClient);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "MORDOCHKA";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClient;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace MORDOCHKA
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+            string connectionString = @"Server=10.10.1.24;Database=PolitDV;User id =pro-41;Password=Pro_41student;TrustServerCertificate =True;";
+            string sql = "Select Clients.ID,Clients.FirstName,Clients.LastName,Clients.Patronymic,Clients.Birthday,Clients.RegistrationDate,Clients.Email,Clients.GenderCode,Clients.PhotoPath,Tag.Title as Tag from Clients, Tag, TagOfClient Where TagOfClient.ClientID = Clients.ID";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Создаем объект DataAdapter
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                // Создаем объект DataSet
+                DataSet ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                dgvClient.DataSource = ds.Tables[0];
+                connection.Close();
+            }
+        }
+
+        private void dgvClient_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            //string connectionString = @"Server=10.10.1.24;Database=PolitDV;User id =pro-41;Password=Pro_41student;TrustServerCertificate =True;";
+            //string sql = "select Tag.Title,Tag.Color from Tag,TagOfClient Where TagOfClient.TagID = Tag.ID";
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    // Создаем объект DataAdapter
+            //    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+            //    // Создаем объект DataSet
+            //    DataSet ds = new DataSet();
+            //    // Заполняем Dataset
+            //    adapter.Fill(ds);
+
+            //}
+
+            for (int i = 0; i < dgvClient.ColumnCount; i++)
+            {
+                bool firstFound = false;
+                for (int j = 0; j < dgvClient.RowCount; j++)
+                {
+                    if (dgvClient[i, j].Value != null && !dgvClient[i, j].Value.Equals("Постоянный") && !firstFound)
+                    {
+                        firstFound = true;
+                    }
+
+                    else if (dgvClient[i, j].Value != null && dgvClient[i, j].Value.ToString() == "Постоянный")
+                    {
+                        dgvClient[i, j].Style.ForeColor = Color.Red;
+                    }
+                }
+            }
+        }
+    }
+}
