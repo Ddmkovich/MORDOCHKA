85dc401eebea92c389b374299f8777f0251ef07f
Ddmkovich <79242132+Ddmkovich@users.noreply.github.com>
2021-12-17 14:27:36 +0700

    Страницы и  начало поиска

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,6 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.dgvClient = new System.Windows.Forms.DataGridView();
+            this.btPrev = new System.Windows.Forms.Button();
+            this.btNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPageSize = new System.Windows.Forms.ComboBox();
+            this.lbRows = new System.Windows.Forms.Label();
+            this.lbAllRows = new System.Windows.Forms.Label();
+            this.tbFiltrName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,27 +46,125 @@
             this.dgvClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvClient.Location = new System.Drawing.Point(12, 12);
             this.dgvClient.Name = "dgvClient";
-            this.dgvClient.Size = new System.Drawing.Size(1200, 598);
+            this.dgvClient.Size = new System.Drawing.Size(1042, 557);
             this.dgvClient.TabIndex = 0;
             this.dgvClient.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvClient_RowsAdded);
+            // 
+            // btPrev
+            // 
+            this.btPrev.Location = new System.Drawing.Point(168, 604);
+            this.btPrev.Name = "btPrev";
+            this.btPrev.Size = new System.Drawing.Size(75, 23);
+            this.btPrev.TabIndex = 1;
+            this.btPrev.Text = "Назад";
+            this.btPrev.UseVisualStyleBackColor = true;
+            this.btPrev.Click += new System.EventHandler(this.btPrev_Click);
+            // 
+            // btNext
+            // 
+            this.btNext.Location = new System.Drawing.Point(168, 575);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(75, 23);
+            this.btNext.TabIndex = 2;
+            this.btNext.Text = "Вперёд";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 585);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество строк";
+            // 
+            // cmbPageSize
+            // 
+            this.cmbPageSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPageSize.FormattingEnabled = true;
+            this.cmbPageSize.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.cmbPageSize.Items.AddRange(new object[] {
+            "10",
+            "50",
+            "200",
+            "Все"});
+            this.cmbPageSize.Location = new System.Drawing.Point(15, 605);
+            this.cmbPageSize.Name = "cmbPageSize";
+            this.cmbPageSize.Size = new System.Drawing.Size(121, 21);
+            this.cmbPageSize.TabIndex = 5;
+            this.cmbPageSize.SelectedIndexChanged += new System.EventHandler(this.cmbPageSizeSelectedIndexChanged);
+            // 
+            // lbRows
+            // 
+            this.lbRows.AutoSize = true;
+            this.lbRows.Location = new System.Drawing.Point(15, 633);
+            this.lbRows.Name = "lbRows";
+            this.lbRows.Size = new System.Drawing.Size(13, 13);
+            this.lbRows.TabIndex = 6;
+            this.lbRows.Text = "0";
+            // 
+            // lbAllRows
+            // 
+            this.lbAllRows.AutoSize = true;
+            this.lbAllRows.Location = new System.Drawing.Point(100, 632);
+            this.lbAllRows.Name = "lbAllRows";
+            this.lbAllRows.Size = new System.Drawing.Size(13, 13);
+            this.lbAllRows.TabIndex = 7;
+            this.lbAllRows.Text = "0";
+            // 
+            // tbFiltrName
+            // 
+            this.tbFiltrName.Location = new System.Drawing.Point(1060, 12);
+            this.tbFiltrName.Name = "tbFiltrName";
+            this.tbFiltrName.Size = new System.Drawing.Size(100, 20);
+            this.tbFiltrName.TabIndex = 8;
+            this.tbFiltrName.TextChanged += new System.EventHandler(this.tbFiltrName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 604);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1224, 691);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFiltrName);
+            this.Controls.Add(this.lbAllRows);
+            this.Controls.Add(this.lbRows);
+            this.Controls.Add(this.cmbPageSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btNext);
+            this.Controls.Add(this.btPrev);
             this.Controls.Add(this.dgvClient);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "MORDOCHKA";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvClient;
+        private System.Windows.Forms.Button btPrev;
+        private System.Windows.Forms.Button btNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPageSize;
+        private System.Windows.Forms.Label lbRows;
+        private System.Windows.Forms.Label lbAllRows;
+        private System.Windows.Forms.TextBox tbFiltrName;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -13,41 +13,28 @@ namespace MORDOCHKA
 {
     public partial class MainForm : Form
     {
+        int pageSize = 10; // размер страницы
+        int pageNumber = 0; // текущая страница
+        int rowCount;
+        int maxRows;
+        DataTable tagTbl = new DataTable();
+        DataSet ds;
+        string connectionString = @"Server=10.10.1.24;Database=PolitDV;User id =pro-41;Password=Pro_41student;TrustServerCertificate =True;";
         public MainForm()
         {
             InitializeComponent();
-
-
-            string connectionString = @"Server=10.10.1.24;Database=PolitDV;User id =pro-41;Password=Pro_41student;TrustServerCertificate =True;";
-            string sql = "Select Clients.ID,Clients.FirstName,Clients.LastName,Clients.Patronymic,Clients.Birthday,Clients.RegistrationDate,Clients.Email,Clients.GenderCode,Clients.PhotoPath,Tag.Title as Tag from Clients, Tag, TagOfClient Where TagOfClient.ClientID = Clients.ID";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                // Создаем объект DataAdapter
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-                // Создаем объект DataSet
-                DataSet ds = new DataSet();
-                // Заполняем Dataset
-                adapter.Fill(ds);
-                dgvClient.DataSource = ds.Tables[0];
-                connection.Close();
-            }
+            dgvClient.SelectionMode =DataGridViewSelectionMode.FullRowSelect;
+            dgvClient.AllowUserToAddRows = false;
+            Query(connectionString, GetSql());
+            rowCount = pageSize;
+            maxRows = Convert.ToInt32(RowCount(connectionString, GetSql()));
+            lbRows.Text = rowCount.ToString();
+            lbAllRows.Text = maxRows.ToString();
         }
 
         private void dgvClient_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
         {
-            //string connectionString = @"Server=10.10.1.24;Database=PolitDV;User id =pro-41;Password=Pro_41student;TrustServerCertificate =True;";
-            //string sql = "select Tag.Title,Tag.Color from Tag,TagOfClient Where TagOfClient.TagID = Tag.ID";
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    // Создаем объект DataAdapter
-            //    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-            //    // Создаем объект DataSet
-            //    DataSet ds = new DataSet();
-            //    // Заполняем Dataset
-            //    adapter.Fill(ds);
-
-            //}
-
+            //Поиск тега в таблице и его окраска в цвет
             for (int i = 0; i < dgvClient.ColumnCount; i++)
             {
                 bool firstFound = false;
@@ -64,6 +51,102 @@ namespace MORDOCHKA
                     }
                 }
             }
+        }
+        private void Query(string connStr,string sqlQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                connection.Open();
+                // Создаем объект DataAdapter
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, connection);
+                // Создаем объект DataSet
+                ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                dgvClient.DataSource = ds.Tables[0];
+                dgvClient.Columns["ID"].ReadOnly = true;
+                connection.Close();
+                
+
+            }
+        }
+        private string RowCount(string connStr, string sqlQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connStr))
+            { 
+                connection.Open();
+                // Создаем объект DataAdapter
+                SqlCommand command = new SqlCommand(MaxId(), connection);
+                // Создаем объект DataSet
+                int reader = (int)command.ExecuteScalar();
+                string maxId = reader.ToString();
+                connection.Close();
+                return maxId;
+
+            }
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPrev_Click(object sender, EventArgs e)
+        {
+            if (pageNumber == 0) return;
+            pageNumber--;
+            rowCount -= pageSize;
+            lbRows.Text = rowCount.ToString();
+            Query(connectionString, GetSql());
+        }
+
+        private void btNext_Click(object sender, EventArgs e)
+        {
+            if (rowCount >= maxRows) return;
+            pageNumber++;
+            rowCount += pageSize;
+            lbRows.Text = rowCount.ToString();
+            Query(connectionString, GetSql());
+        }
+        private string GetSql()
+        {
+            return "SELECT * FROM Clients ORDER BY Id OFFSET ((" + pageNumber + ") * " + pageSize + ") " +
+                "ROWS FETCH NEXT " + pageSize + "ROWS ONLY";
+        }
+        private string MaxId()
+        {
+            return "SELECT Count(*) FROM Clients";
+        }
+        private void cmbPageSizeSelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (cmbPageSize.Text!="Все")
+            {
+                pageSize = Convert.ToInt32(cmbPageSize.Text);
+                pageNumber = 0;
+                rowCount = pageSize;
+                if (rowCount>maxRows)
+                {
+                    rowCount = maxRows;
+                }
+                lbRows.Text = rowCount.ToString();
+                Query(connectionString, GetSql());
+            }
+            else
+            {
+                pageSize = 10000;
+                pageNumber = 0;
+                rowCount = pageSize;
+                lbRows.Text = rowCount.ToString();
+                Query(connectionString, GetSql());
+            }
+            
+        }
+
+        private void tbFiltrName_TextChanged(object sender, EventArgs e)
+        {
+            (dgvClient.DataSource as DataTable).DefaultView.RowFilter =
+            String.Format("FirstName like '{0}%'", tbFiltrName.Text);
         }
     }
 }
